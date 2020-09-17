b77a4bb101dbd0ce0bee19cd322de6ee8ba737f6
Sebastian Pea <juansebastianp21@gmail.com>
2020-09-17 00:50:10 -0500

    seleccion de imagen de perfil

--- reducers/reducerPeliculas.js
+++ reducers/reducerPeliculas.js
@@ -1,11 +1,20 @@
-function reducer(state = {}, action){
+const initialState = {
+    favoritos: []
+}
+
+function reducer(state = initialState, action){
     switch(action.type){
         case 'SET_PELICULAS_LISTAS':{
             return {
                 ...state, ...action.payload 
             }
         }
-      
+        case 'ADD-FAVORITO': {
+            return {
+                ...state,
+                favoritos: [...state.favoritos, action.payload.movie],
+            }
+        }
         default:
             return state
     };
